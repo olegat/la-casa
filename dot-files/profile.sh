78fee812c81a933246d53c8be52115803298f6bc
Oli Legat <olegat@google.com>
2018-12-05 14:08:16 +0000

    Set macOS PS1, move YetiSDK to common profile.sh

--- dot-files/profile.sh
+++ dot-files/profile.sh
@@ -2,3 +2,9 @@
 if  [ -f "$(dirname ${BASH_SOURCE[0]})/profile-$(uname).sh" ] ; then
   source "$(dirname ${BASH_SOURCE[0]})/profile-$(uname).sh"
 fi
+
+#------------------------------------------------------------------------------
+# Yeti GGP CLI (Unix)
+#------------------------------------------------------------------------------
+export YETI_SDK_PATH=${HOME}/YetiSDK
+export PATH=${PATH}:${HOME}/YetiSDK/dev/bin
