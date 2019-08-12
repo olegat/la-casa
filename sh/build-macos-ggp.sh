405ccf75c3d5eadc10f9bf4212866cceec512373
Oli Legat <olegat@google.com>
2019-08-12 14:47:51 +0100

    Typo chrome_client -> chromeclient

--- sh/build-macos-ggp.sh
+++ sh/build-macos-ggp.sh
@@ -33,12 +33,12 @@ popd > /dev/null
 #------------------------------------------------------------------------------
 # Rename / Re-structure hierarchy.
 rm -fr "${TMPDIR}"
-mkdir -p "/${TMPDIR}/sdk/dev/bin/chrome_client"
+mkdir -p "/${TMPDIR}/sdk/dev/bin/chromeclient"
 cp "${GOOGLE3}/blaze-bin/chrome/cloudcast/tools/cli/ggp_darwin" \
   "${TMPDIR}/sdk/dev/bin/ggp"
 
 cp "${GOOGLE3}/blaze-bin/chrome/cloudcast/devchromefe/chrome_client/chrome_client_darwin" \
-  "${TMPDIR}/sdk/dev/bin/chrome_client/chrome_client"
+  "${TMPDIR}/sdk/dev/bin/chromeclient/chrome_client"
 
 # Create tarball
 pushd "/${TMPDIR}/sdk" > /dev/null
