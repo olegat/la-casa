597f8d9f249486997051eda262342e77f8fa730f
Oli Legat <olegat@google.com>
2019-10-29 15:34:01 +0000

    Move findCaseNumber() back into background.js

--- experimental/stadia-sf-chrome-extension/background.js
+++ experimental/stadia-sf-chrome-extension/background.js
@@ -12,9 +12,41 @@ function promiseGetURLWithoutQuery() {
     });
 }
 
+function findCaseNumber() {
+  /** Salesforce Renders the HTML like.
+      <div>
+        <div>
+  (1)     <span>Case Number</span>
+        </div>
+  (2)   <div>
+          <span><span>00000001</span></span>
+        </div>
+      </div>
+  */ 
+  // Search for node (1)
+  let xpath = '//span[contains(., "Case Number")]'
+  let label = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+
+  // Get first common ancestor of (1) and (2)
+  let x = label.iterateNext().parentNode.parentNode;
+
+  // Search for node (2)
+  xpath = './/span'
+  var value = document.evaluate(xpath, x, null, XPathResult.ANY_TYPE, null)
+  value.iterateNext()
+
+  return String(value.iterateNext().textContent)
+}
+
 function promiseGetCaseNumber() {
   return new Promise(function(resolve,reject){
-    chrome.tabs.executeScript(null, {code: 'findCaseNumber()'}, resolve)
+    chrome.tabs.executeScript(
+      null,
+      {
+        // TODO(olegat) see if possible to inject this function permanently
+        code: findCaseNumber.toString() + 'findCaseNumber()'
+      },
+      resolve)
   })
 }
 
