06341b90cd2567c225e3df5a4c3e9d3cd024f179
Oli Legat <olegat@google.com>
2020-02-14 12:51:08 +0000

    Bugfix: use GIT_ROOT instead of absolute path

--- ps1/Scripts/Init-Git-Unix-Environment.ps1
+++ ps1/Scripts/Init-Git-Unix-Environment.ps1
@@ -1,10 +1,9 @@
 # Load this file using ". Init-Git-Unix-Environment.ps1"
 #
-# TODO(oli.legat) - fix hard-coded path  
-$Env:PATH += "C:\Users\olegat\AppData\Local\Programs\Git\usr\bin;"
+$Env:PATH += "${Env:GIT_ROOT}\usr\bin;"
 
 # Use ls.exe instead of Get-ChildItem 
 function Git-ls-autocolor {
-  C:\Users\olegat\AppData\Local\Programs\Git\usr\bin\ls.exe --color=auto $args
+  &"${Env:GIT_ROOT}\usr\bin\ls.exe" --color=auto $args
 }
 Set-Alias -Name ls -Value Git-ls-autocolor -Option AllScope
