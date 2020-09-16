0ad5364c1120bd3c748b7a3de1fc3e07fdfc51d1
Oli Legat <olegat@google.com>
2020-09-16 13:03:25 +0100

    Add MikTex to PATH for OpenABM-env

--- cmd/OpenABM-env.bat
+++ cmd/OpenABM-env.bat
@@ -1,4 +1,4 @@
 @call R-env.bat
-@set PATH=%PATH%;C:\cygwin64\bin;
+@set PATH=%PATH%;C:\Users\olegat\AppData\Local\Programs\MiKTeX\miktex\bin\x64;C:\cygwin64\bin;
 @set PATH=%PATH:;;=;%
 cd C:\Users\olegat\github.com\BDI-pathogens\OpenABM-Covid19
