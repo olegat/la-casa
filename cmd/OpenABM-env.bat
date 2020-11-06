df04d09c8e15fd2af6e50e8d3c9d568d6cdaff2c
Oli Legat <olegat@google.com>
2020-11-06 11:35:21 +0000

    Update R version 3.6.2 -> 3.6.3

--- cmd/OpenABM-env.bat
+++ cmd/OpenABM-env.bat
@@ -1,4 +1,5 @@
 @call R-env.bat
 @set PATH=%PATH%;C:\Users\olegat\AppData\Local\Programs\MiKTeX\miktex\bin\x64;C:\cygwin64\bin;
 @set PATH=%PATH:;;=;%
-cd C:\Users\olegat\github.com\BDI-pathogens\OpenABM-Covid19
+@set PROMPT=$N:$G$S
+@cd C:\Users\olegat\github.com\BDI-pathogens\OpenABM-Covid19
--- cmd/R-env.bat
+++ cmd/R-env.bat
@@ -1,3 +1,3 @@
 @call base-env.bat
-@set PATH=C:\Rtools\bin;C:\Program Files\R\R-3.6.2\bin\x64;%PATH%
+@set PATH=C:\Rtools\bin;C:\Program Files\R\R-3.6.3\bin\x64;%PATH%
 @set PATH=%PATH:;;=;%
