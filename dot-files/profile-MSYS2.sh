dc838d74c6f9c4c7c26a89660dd383afb8b01663
Oli Legat <olegat@google.com>
2021-02-03 18:07:19 +0000

    emacs.bat: Support spaces in EMACS_ROOT

--- dot-files/profile-MSYS2.sh
+++ dot-files/profile-MSYS2.sh
@@ -3,5 +3,5 @@
 emacs() {
   # TODO: $@ will not work for spaces and fullpaths.
   #       It should use "" and `cygpath -w` but good enough for now.
-  cmd /c "cmd /c $EMACS_ROOT\\bin\\runemacs.exe $@"
+  cmd /c "cmd /c \"$EMACS_ROOT\\bin\\runemacs.exe\" $@"
 }
