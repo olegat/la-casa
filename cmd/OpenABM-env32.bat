81f619c9e10bb7e29af733a63c43351f97b38643
Oli Legat <olegat@google.com>
2021-03-09 15:00:29 +0000

    Update OpenABM-env*.bat and add dos2unix wrapper

--- cmd/OpenABM-env32.bat
+++ cmd/OpenABM-env32.bat
@@ -1,5 +1,6 @@
 @call R32-env.bat
-@set PATH=%PATH%;C:\Users\olegat\AppData\Local\Programs\MiKTeX\miktex\bin\x64;C:\cygwin64\bin;
+@set PATH=%PATH%;C:\Users\olegat\AppData\Local\Programs\MiKTeX\miktex\bin\x64;
 @set PATH=%PATH:;;=;%
+@set SWIG3=C:\cygwin64\bin\swig.exe
 @set PROMPT=$N:$G$S
 @cd C:\Users\olegat\github.com\BDI-pathogens\OpenABM-Covid19
