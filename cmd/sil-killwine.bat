50afc9d2918a33491557e70eb9b9b111d4572c8d
Oli Legat <olegat@google.com>
2021-08-27 17:08:57 +0100

    Unused silenus batch scripts

---
+++ cmd/sil-killwine.bat
@@ -0,0 +1,8 @@
+ggp ssh shell pkill wineserver
+ggp ssh shell pkill winedbg
+ggp ssh shell pkill C:\windows\system32\winedevice.exe
+ggp ssh shell pkill C:\windows\system32\plugplay.exe
+ggp ssh shell pkill C:\windows\system32\svchost.exe
+ggp ssh shell pkill C:\windows\system32\explorer.exe
+ggp ssh shell pkill C:\windows\system32\rpcss.exe
+ggp ssh shell pkill dx11_fishtornado.exe
