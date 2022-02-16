842aacf419d9ad8774d65aba5367a851c1c000c5
Oli Legat <olegat@google.com>
2022-02-16 17:15:30 +0000

    Add template .bashrc for Silenus Docker shell

--- silenus/bashrc.sh
+++ silenus/bashrc.sh
@@ -1,10 +1,6 @@
-if [ -f ~/.bashrc ] ; then
-  . ~/.bashrc
-fi
+# Save to ~/.silenus-local/.bashrc
+# Use la-casa/silenus/install_emacs.sh to install Emacs
 
-cd /workspace/src/silenus
-
-# Use docker/install_emacs.sh to install this.
 # FIXME(olegat) USER=dockerbuilder causes emacs to not load ~/.emacs
 # FIXME(olegat) Using the same .emacs.d dir as google3 emacs causes clashes.
 #   Setting HOME telling emacs to use a different directory. ~/.vimrc causes
@@ -12,8 +8,3 @@ cd /workspace/src/silenus
 alias emacs="HOME=/workspace/local USER= /workspace/local/bin/emacs"
 alias emacsclient="HOME=/workspace/local USER= /workspace/local/bin/emacsclient"
 alias vim="HOME=/workspace/local /usr/bin/vim"
-
-# Welcome message:
-echo -e '\n  Welcome to the Silenus Docker shell.\n'
-cat /workspace/src/silenus/docker/wineglass.utf8.ansi
-echo ''
