de285ef2979c3c5750fdb4817b4d8ba39ef408bc
Oli Legat <olegat@google.com>
2022-12-01 17:33:32 +0000

    Add ~/.cargo/env to Bash profile

--- dot-files/profile.sh
+++ dot-files/profile.sh
@@ -3,6 +3,10 @@ if  [ -f "$(dirname ${BASH_SOURCE[0]})/profile-$(uname).sh" ] ; then
   source "$(dirname ${BASH_SOURCE[0]})/profile-$(uname).sh"
 fi
 
+if  [ -f "${HOME}/.cargo/env" ] ; then
+  source "${HOME}/.cargo/env"
+fi
+
 #------------------------------------------------------------------------------
 # GGP CLI (Unix)
 #------------------------------------------------------------------------------
