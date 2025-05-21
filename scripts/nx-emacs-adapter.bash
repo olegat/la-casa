7ea5c61ba3944782ee5087bd752df5f6a2e52eb0
Oli Legat <olivier.legat@ag-grid.com>
2025-05-21 14:16:48 +0100

    add ag-charts-generate-code-reference-files to nx-emacs-adapter.bash

--- scripts/nx-emacs-adapter.bash
+++ scripts/nx-emacs-adapter.bash
@@ -11,12 +11,16 @@ export NVM_DIR="$HOME/.nvm"
 
 { nx "$@" \
      2> >(sed -e 's/ag-charts-types: \(.*\)/\1/g' \
+              -e 's/ag-charts-generate-code-reference-files: \(.*\)/\1/g' \
+              -e 's/ag-charts-core: \(.*\)/\1/g' \
               -e 's/ag-charts-community: \(.*\)/\1/g' \
               -e 's/ag-charts-enterprise: \(.*\)/\1/g' \
               -e 's/ag-charts-angular: \(.*\)/\1/g' \
               -e 's/ag-charts-vue3: \(.*\)/\1/g' \
               -e 's/ag-charts-react: \(.*\)/\1/g') \
       | (sed -e 's/ag-charts-types: \(.*\)/\1/g' \
+             -e 's/ag-charts-generate-code-reference-files: \(.*\)/\1/g' \
+             -e 's/ag-charts-core: \(.*\)/\1/g' \
              -e 's/ag-charts-community: \(.*\)/\1/g' \
              -e 's/ag-charts-enterprise: \(.*\)/\1/g' \
              -e 's/ag-charts-angular: \(.*\)/\1/g' \
